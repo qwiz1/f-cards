038608477eea9ea7e43cc6958adba0597c5b56cc
Andrii Solohub <andrei.sologub12@gmail.com>
2023-04-14 21:18:01 +0300

    add users module

---
+++ server/src/users/users.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { User } from './entities/user.entity';
+import { Role } from 'src/roles/entities/role.entity';
+import { RolesModule } from 'src/roles/roles.module';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User, Role]), RolesModule],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService],
+})
+export class UsersModule {}
