de5f1dc30c3546bbf385125a034ce337daa5473a
Andrii Solohub <andrei.sologub12@gmail.com>
2023-04-14 21:25:43 +0300

    add role controller

---
+++ server/src/roles/roles.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { RolesService } from './roles.service';
+import { CreateRoleDto } from './dto/create-role.dto';
+
+@Controller('roles')
+export class RolesController {
+  constructor(private readonly rolesService: RolesService) {}
+
+  @Post()
+  create(@Body() createRoleDto: CreateRoleDto) {
+    return this.rolesService.create(createRoleDto);
+  }
+
+  @Get()
+  getAll() {
+    return this.rolesService.getAll();
+  }
+
+  @Get(':value')
+  getRoleByValue(@Param('value') value: string) {
+    return this.rolesService.getRoleByValue(value);
+  }
+}
