a75e935b7c2f6b0c003c14c71f950fe3bec6f45c
Andrii Solohub <andrei.sologub12@gmail.com>
2023-04-14 21:16:12 +0300

    add user entity

---
+++ server/src/users/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import { Role } from 'src/roles/entities/role.entity';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @ManyToMany(() => Role)
+  @JoinTable()
+  roles: Role[];
+}
