8cc4e17fd0732db029f7cd72db3af24ce9d755ca
Andrii Solohub <andrei.sologub12@gmail.com>
2023-04-14 21:32:32 +0300

    + importing modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { AuthModule } from './auth/auth.module';
+import { RolesModule } from './roles/roles.module';
+import { Role } from './roles/entities/role.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'adminadmin',
+      database: 'f-card-db',
+      entities: [User, Role],
+      synchronize: true,
+    }),
+    UsersModule,
+    AuthModule,
+    RolesModule,
+  ],
 })
 export class AppModule {}
