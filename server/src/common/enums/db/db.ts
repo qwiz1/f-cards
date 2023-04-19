1203fe27442ed5c5cede3b148e89b5ea9977cd19
Andrii Solohub <andrei.sologub12@gmail.com>
2023-04-19 17:36:39 +0300

    + centralized export

--- server/src/common/enums/db/db.ts
+++ server/src/common/enums/db/db.ts
@@ -1,5 +1,7 @@
-import { RoleDTOKey } from './role-dto-key.enum';
 import { TableName } from './table-name.enum';
 import { UserDTOKey } from './user-dto-key.enum';
+import { RoleDTOKey } from './role-dto-key.enum';
+import { DeckDTOKey } from './deck-dto-key.enum';
+import { CardDTOKey } from './card-dto-key.enum';
 
-export { TableName, UserDTOKey, RoleDTOKey };
+export { TableName, UserDTOKey, RoleDTOKey, DeckDTOKey, CardDTOKey };
