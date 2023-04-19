908a9099e59cf4df585613fd2ffdc0a9753e4e0d
Andrii Solohub <andrei.sologub12@gmail.com>
2023-04-19 17:42:32 +0300

    inject new entities

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,11 +4,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { RolesModule } from './roles/roles.module';
+import { DecksModule } from './decks/decks.module';
+import { CardsModule } from './cards/cards.module';
 import { User } from './users/entities/user.entity';
 import { Role } from './roles/entities/role.entity';
+import { Card } from './cards/entities/card.entity';
+import { Deck } from './decks/entities/deck.entity';
 import { UsersController } from './users/users.controller';
 import { AuthController } from './auth/auth.controller';
 import { RolesController } from './roles/roles.controller';
+import { DecksController } from './decks/decks.controller';
 import { LoggerMiddleware } from './middlewares/middlewares';
 
 @Module({
@@ -21,18 +26,25 @@ import { LoggerMiddleware } from './middlewares/middlewares';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      entities: [User, Role],
+      entities: [User, Role, Card, Deck],
       synchronize: true,
     }),
     UsersModule,
     AuthModule,
     RolesModule,
+    CardsModule,
+    DecksModule,
   ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(LoggerMiddleware)
-      .forRoutes(AuthController, UsersController, RolesController);
+      .forRoutes(
+        AuthController,
+        UsersController,
+        RolesController,
+        DecksController,
+      );
   }
 }
